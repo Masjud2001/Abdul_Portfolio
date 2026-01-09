c42129ed4f08a067ee0331de38ff9bfeab60f468
Masjud2001 <jitislam340@gmail.com>
2026-01-08 19:05:18 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 const text = "Azure | SOC Analyst | Cloud Security | Docker";
 let index = 0;
-
-function typingEffect() {
-  if (index < text.length) {
+function typingEffect(){
+  if(index < text.length){
     document.getElementById("typing").innerHTML += text.charAt(index);
     index++;
-    setTimeout(typingEffect, 70);
+    setTimeout(typingEffect,70);
   }
 }
-
 typingEffect();
